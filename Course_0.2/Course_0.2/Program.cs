30ab3476a6241919a90d368047e74e7b58a5d7c0
Rebeca <rebeca.nonato.lemaf@gmail.com>
2020-07-10 19:42:31 -0300

    projeto com OO

---
+++ Course_0.2/Course_0.2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course_0._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Triangulo x, y;
+            x = new Triangulo();
+            y = new Triangulo();
+
+
+            Console.WriteLine("Entre com as medidas do tringulo X: ");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do tringulo Y: ");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double areaX = x.CalculaArea();
+            double areaY = y.CalculaArea();
+
+            Console.WriteLine("Area de X: " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Area de Y: " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Maior área é a X. ");
+            }
+            else
+            {
+                Console.WriteLine("Maior área é a Y. ");
+            }
+        }
+    }
+}
---
+++ Course_0.2/Course_0.2/Triangulo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Course_0._2
+{
+    class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+
+
+        public double CalculaArea()
+        {
+            double valorArea = 2.0;
+
+            double p = (A + B + C) / valorArea;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
+    }
+}
