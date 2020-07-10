2f17f5a8c213f11832d6a8beea907d5e63249aeb
Rebeca <rebeca.nonato.lemaf@gmail.com>
2020-07-10 19:26:01 -0300

    first commit

---
+++ Course_0.1/Course_0.1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course_0._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double xA, xB, xC, yA, yB, yC;
+            double p;
+            double areaX, areaY;
+
+            Console.WriteLine("Entre com as medidas do tringulo X: ");
+            xA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            xB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do tringulo Y: ");
+            yA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            p = (xA + xB + xC) / 2.0;
+            areaX = Math.Sqrt(p * (p - xA) * (p - xB) * (p - xC));
+            p = (yA + yB + yC) / 2.0;
+            areaY = Math.Sqrt(p * (p - yA) * (p - yB) * (p - yC));
+
+            Console.WriteLine("Area de X: " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Area de Y: " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if(areaX > areaY)
+            {
+                Console.WriteLine("Maior área é a X. ");
+            }
+            else
+            {
+                Console.WriteLine("Maior área é a Y. ");
+            }
+
+        }
+    }
+}
