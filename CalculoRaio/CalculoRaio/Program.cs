4c18fdb61becc0c8feba8768ce1d7da443c0a43a
Rebeca <rebeca.nonato.lemaf@gmail.com>
2020-08-19 13:50:51 -0300

    static

---
+++ CalculoRaio/CalculoRaio/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace CalculoRaio
+{
+    class Program
+    {
+        //static double Pi = 3.14;
+
+        static void Main(string[] args)
+        {
+
+            Console.Write("Entre com o valor do Raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Cifcunferência: " + Calculadora.Circunferencia(raio).ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + Calculadora.Volume(raio).ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+
+        //static double Circunferencia(double r)
+        //{
+        //    return 2.0 * Pi * r;
+        //}
+
+        //static double Volume(double r)
+        //{
+        //    return 4.0 / 3.0 * Pi * Math.Pow(r, 3.0);
+        //}
+    }
+}
