1375444a71a2d1517b65c1a60c6221671d63d425
rebecanonato <rebecanonato89@gmail.com>
2021-01-24 16:59:33 -0300

    desafios

---
+++ Desafios DIO/Distancia/Distancia/Desafio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Distancia
+{
+    class Desafio
+    {
+        static void Main()
+        {
+            int quilometros = Int32.Parse(Console.ReadLine());
+            int minutos = quilometros * 2; // Digite aqui o calculo dos minutos
+            Console.WriteLine(minutos + " minutos");
+        }
+    }
+}
