af3330f0bffca4114182fe050a3bf1e2d30252f9
Rebeca Nonato Domingos <rebecanonato89@gmail.com>
2021-09-16 10:54:33 -0300

    Create TempoDoDobby.cs

---
+++ TempoDoDobby.cs
@@ -0,0 +1,21 @@
+using System;
+
+class MinhaClasse {
+   public static void Main (string[] args) 
+    {
+    int n = int.Parse(Console.ReadLine());
+    string[] texto = Console.ReadLine().Split(" ");
+    
+    int x = int.Parse(texto[0]), y = int.Parse(texto[1]);
+    int calc = x + y;
+    
+    if (calc > n) 
+    {
+      Console.Write("Deixa para amanha!");
+    } 
+    else 
+    {
+      Console.Write("Farei hoje!");
+    }   
+  }
+}
